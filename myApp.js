89a2604c30dc581f5810179f94b836045a7be467
Shubh S <signin2new@gmail.com>
2024-02-25 08:46:29 +0000

    HTML File Served via Express

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 let express = require('express');
 let app = express();
+console.log("Hello World");
 
-
+app.get("/",(req, res)=> {
+    //res.send("Hello Express");
+    let absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
+});
 
 
 
