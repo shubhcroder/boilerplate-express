823f368b760db69f6751d4f817fcdaf700a05419
Shubh S <signin2new@gmail.com>
2024-02-25 12:46:47 +0000

    Environment Variables

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 let express = require('express');
 let app = express();
 console.log("Hello World");
@@ -8,10 +10,17 @@ app.get("/",(req, res)=> {
     res.sendFile(absolutePath);
 });
 
+app.use("/public", express.static(__dirname + '/public'));
 
-
-
-
+app.get("/json", (req,res) => {
+    let message = process.env.MESSAGE_STYLE;
+    console.log(message);
+    if(message=="uppercase"){
+        res.json({"message": "HELLO JSON"});
+    }
+    else
+        res.json({"message": "Hello json"});
+});
 
 
 
