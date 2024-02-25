0bb726b14743798c420f607f330fa00392f9faff
Shubh S <signin2new@gmail.com>
2024-02-25 17:21:11 +0000

    basic node and express modules

--- myApp.js
+++ myApp.js
@@ -1,8 +1,28 @@
 require('dotenv').config()
 
+let bodyParser = require('body-parser');
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
 console.log("Hello World");
+
+app.use(
+    bodyParser.urlencoded({ extended: false }),
+    bodyParser.json()
+);
+
+app.route("/name").get((req, res, next) => {
+    let firstName = req.query.first;
+    let lastName = req.query.last;
+    res.send({"name": `${firstName} ${lastName}`});
+    next();
+})
+.post((req, res) => {
+    console.log(req.body.first, req.body.last);
+    res.json({
+        name: `${req.body.first} ${req.body.last}`
+    });
+});
 
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
@@ -39,9 +59,18 @@ app.get("/json", (req,res) => {
     }
 });
 
+app.get("/:word/echo", (req, res, next) => {
+    let wordParam = req.params.word;
+    res.send({"echo":wordParam});
+    next();
+});
 
-
-
+app.get("/name", (req, res, next) => {
+    let firstName = req.query.first;
+    let lastName = req.query.last;
+    res.send({"name": `${firstName} ${lastName}`});
+    next();
+});
 
 
 
